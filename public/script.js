775f88fcb295a36080885ca38cb0188fdb24372e
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-07-31 15:20:41 -0700

    Create getAllBooks functionality

--- public/script.js
+++ public/script.js
@@ -1,7 +1,33 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  const baseURL = 'http://mutably.herokuapp.com'
 
-  // code in here
+  getAllBooks(baseURL)
 
+  // Create the html grid system for each of the returning books.
 });
+
+
+
+
+const getAllBooks = (baseURL) => {
+  $.ajax({
+    url: `${baseURL}/books`,
+    contentType: 'application/json',
+    success: (response) => {
+      const list = $('.list-group')[0]
+      response.books.forEach((book) => {
+        $('.list-group').append(`
+          <li class='list-group-item'>${book.title}
+            <div class='list-btns'>
+              <button type='button' class='btn btn-default'>View</button>
+              <button type='button' class='btn btn-default'>Edit</button>
+              <button type='button' class='btn btn-default'>Delete</button>
+            </div
+          </li>
+        `)
+      })
+    }
+  })
+}
