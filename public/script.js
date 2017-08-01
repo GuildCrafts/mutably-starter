a33f93f0d38de64d99a848d6e8eaa80a012d7cd9
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-07-31 17:16:50 -0700

    View individual books

--- public/script.js
+++ public/script.js
@@ -5,7 +5,16 @@ $(document).ready(function(){
 
   getAllBooks(baseURL)
 
-  // Create the html grid system for each of the returning books.
+  $(document).on('click', '.view-btn', function() {
+    const bookId = $(this).closest('li').attr('id')
+    const selectedListItem = $(this).closest('li')
+
+    getOneBook(baseURL, bookId, selectedListItem)
+  })
+
+  $(document).on('click', '.close-btn', function() {
+    $(this).closest('div.card').remove()
+  })
 });
 
 
@@ -15,19 +24,51 @@ const getAllBooks = (baseURL) => {
   $.ajax({
     url: `${baseURL}/books`,
     contentType: 'application/json',
-    success: (response) => {
-      const list = $('.list-group')[0]
-      response.books.forEach((book) => {
-        $('.list-group').append(`
-          <li class='list-group-item'>${book.title}
-            <div class='list-btns'>
-              <button type='button' class='btn btn-default'>View</button>
-              <button type='button' class='btn btn-default'>Edit</button>
-              <button type='button' class='btn btn-default'>Delete</button>
-            </div
-          </li>
-        `)
-      })
+    success: function(response) {
+      createListElement(response.books)
+    },
+    error: (error) => {
+      console.error(error.message)
     }
   })
+}
+
+const createListElement = (books) => {
+  books.forEach(function(book) {
+    $('.list-group').append(`
+      <li class='list-group-item' id=${book._id}>${book.title}
+        <div class='list-btns'>
+          <button type='button' class='btn btn-default view-btn'>View</button>
+          <button type='button' class='btn btn-default edit-btn'>Edit</button>
+          <button type='button' class='btn btn-default delete-btn'>Delete</button>
+        </div>
+      </li>
+    `)
+  })
+}
+
+const getOneBook = (baseURL, bookId, selectedListItem) => {
+  $.ajax({
+    url: `${baseURL}/books/${bookId}`,
+    contentType: 'application/json',
+    success: function(book) {
+      viewBookDetails(book, selectedListItem)
+    },
+    error: function(error) {
+      console.error(error.message)
+    }
+  })
+}
+
+const viewBookDetails = (book, selectedListItem) => {
+  selectedListItem.after(`
+    <div class="card">
+    <img class="thumbnail" src=${book.image} alt="Card image cap" height='100%' width='120px'>
+    <div class="card-block">
+      <h2 class="card-title">${book.title}</h2>
+      <p class="card-text">Author: ${book.author}</p>
+      <p class="card-text">Release Date: ${book.releaseDate}</p>
+      <button class="btn btn-primary close-btn">Close</button>
+    </div>
+  `)
 }
