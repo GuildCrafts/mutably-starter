93e5a6273f4beb7747ecdee0a4aea0cdd1f800a9
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-08-02 15:31:25 -0700

    All functionality working

--- public/script.js
+++ public/script.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
 
   $(document).on('click', '.close-btn', function() {
     $(this).closest('div.show-card').remove()
-    console.log($(document).find('.show-card'));
   })
 
   $(document).on('click', '.delete-btn', function() {
@@ -34,7 +33,7 @@ $(document).ready(function(){
 
   $(document).on('click', '.submit-edit-btn', function(event) {
     event.preventDefault()
-    const bookId = $(this).closest('div.edit-card').prev().prev().attr('id')
+    const bookId = $(this).closest('div.edit-card').prev().attr('id')
     const image = $(this).closest('div.edit-card').children().children('.image-holder').children().attr('src')
     const title = $(this).parent().parent().find('input#title').val()
     const author = $(this).parent().parent().find('input#author').val()
@@ -45,12 +44,28 @@ $(document).ready(function(){
       releaseDate,
       image
     }
-    $(this).closest('div.edit-card').toggleClass('hide-card')
     editBookDetails(bookId, details)
+    $(document).find('div.edit-card').remove()
   })
 
   $(document).on('click', '.add-btn', function() {
     $('#myModal').modal('show');
+  })
+
+  $(document).on('click', '.add-new-submit', function(event) {
+    event.preventDefault()
+    const image = 'https://sequinsandcherryblossom.files.wordpress.com/2014/12/guest-cat-cover.jpg'
+    const title = $(this).parent().prev().find('input#title').val()
+    const author = $(this).parent().prev().find('input#author').val()
+    const releaseDate = $(this).parent().prev().find('input#date').val()
+    const details = {
+      title,
+      author,
+      releaseDate,
+      image
+    }
+
+    addNewBook(details)
   })
 })
 
@@ -62,7 +77,7 @@ const getAllBooks = () => {
     method: 'get'
   })
   .done(function(response) {
-    createListElements(response.books)
+    populateList(response.books)
   })
   .catch(error => {
     alert('Oh no! Something went wrong. Please try again.')
@@ -86,28 +101,34 @@ const getOneBook = (btn, bookId) => {
 }
 
 // Create a new list element for each book
-const createListElements = (books) => {
+const populateList = (books) => {
   books.forEach(function(book) {
-    $('.list-group').append(`
-      <li class='list-group-item' id=${book._id}>${book.title}
-        <div class='list-btns'>
-          <button type='button' class='btn btn-default view-btn'>View</button>
-          <button type='button' class='btn btn-default edit-btn'>Edit</button>
-          <button type='button' class='btn btn-default delete-btn'>Delete</button>
-        </div>
-      </li>
-    `)
+    createListItem(book)
   })
 }
+
+const createListItem = (book) => {
+  $('.list-group').append(`
+    <li class='list-group-item' id=${book._id}>
+      <p class='list-title'>${book.title}</p>
+      <div class='list-btns'>
+        <button type='button' class='btn btn-default view-btn'>View</button>
+        <button type='button' class='btn btn-default edit-btn'>Edit</button>
+        <button type='button' class='btn btn-default delete-btn'>Delete</button>
+      </div>
+    </li>
+  `)
+}
+
 // Dynamically add a view card for the fetched book
 const createViewBookCard = (book) => {
   $(document).find(`#${book._id}`).after(`
     <div class='show-card'>
       <div class='row'>
-        <div class='col-sm-3 image-holder'>
+        <div class='col-sm-3 image-box'>
           <img class='thumbnail' src=${book.image} alt='Book Cover Image' height='230px' width='160px'>
         </div>
-        <div class='col-sm-9'>
+        <div class='col-sm-9 details-box'>
           <div class=card-block>
             <h2 class='card-title'>${book.title}</h2>
             <p class='card-text'>Author: ${book.author}</p>
@@ -160,6 +181,17 @@ const createEditBookCard = (book) => {
   `)
 }
 
+const updateSingleListElement = (bookId, updatedBook) => {
+  $(document).find(`#${bookId}`).html(`
+    <p class='list-title'>${updatedBook.title}</p>
+    <div class='list-btns'>
+      <button type='button' class='btn btn-default view-btn'>View</button>
+      <button type='button' class='btn btn-default edit-btn'>Edit</button>
+      <button type='button' class='btn btn-default delete-btn'>Delete</button>
+    </div>
+  `).attr(updatedBook._id)
+}
+
 // Delete book entry from the page
 const deleteOneBook = (bookId) => {
   $.ajax({
@@ -191,8 +223,9 @@ const editBookDetails = (bookId, updatedDetails) => {
     method: 'put',
     data: JSON.stringify(updatedDetails)
   })
-  .done(function(response) {
+  .done(function(updatedBook) {
     alert('Book updated successfully!')
+    updateSingleListElement(bookId, updatedBook)
   })
   .catch(error => {
     console.error('Error: ', error)
@@ -200,52 +233,20 @@ const editBookDetails = (bookId, updatedDetails) => {
   })
 }
 
-//  <div class='edit-card hide-card'>
-//   <div class='row'>
-//     <div class='col-sm-3 image-holder'>
-//       <img class='thumbnail' src=${book.image} alt='Book Cover Image' height='230px' width='160px'>
-//     </div>
-//     <div class='col-sm-9'>
-//       <div class=card-block>
-//         <h2 class='card-title'>${book.title}</h2>
-//         <p class='card-text'>Author: ${book.author}</p>
-//         <p class='card-text'>Release Date: ${book.releaseDate}</p>
-//         <button class='btn btn-primary close-btn'>Close</button>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-// <div class='edit-card hide-card'>
-//   <div class='row'>
-//     <div class='col-sm-3 image-holder'>
-//       <img class='thumbnail' src=${book.image} alt='Card image cap' height='230px' width='160px'>
-//     </div>
-//     <div class='col-sm-9'>
-//       <h2>Edit book details</h2>
-//       <form class='edit-form'>
-//         <div class='input-section'>
-//           <label for='title' class='col-sm-3 col-form-label'>Title</label>
-//           <div class='form-input col-sm-9'>
-//             <input type='text' class='form-control' id='title' value="${book.title}"'>
-//           </div>
-//          </div>
-//         <div class='input-section'>
-//           <label for='author' class='col-sm-3 col-form-label'>Author</label>
-//           <div class='form-input col-sm-9'>
-//             <input type='text' class='form-control' id='author' value="${book.author}"'>
-//           </div>
-//          </div>
-//         <div class='input-section'>
-//           <label for='edit-date' class='col-sm-3 col-form-label'>Release Date</label>
-//           <div class='form-input col-sm-9'>
-//             <input type='text' class='form-control' id='date' value="${book.releaseDate}"'>
-//           </div>
-//         </div>
-//         <div class='edit-btns'>
-//           <button type='submit' class='btn btn-primary form-btn upload-btn'>Upload Image</button>
-//           <button type='submit' class='btn btn-primary form-btn submit-edit-btn'>Submit</button>
-//         </div>
-//       </form>
-//     </div>
-//   </div>
-// </div>
+// Add a new book
+const addNewBook = (bookDetails) => {
+  $.ajax({
+    url: `${baseURL}/books`,
+    contentType: 'application/json',
+    method: 'post',
+    data: JSON.stringify(bookDetails)
+  })
+  .done(function(savedBook) {
+    createListItem(savedBook)
+    $('#myModal').modal('hide')
+  })
+  .catch(error => {
+    console.error('Error: ', error)
+    alert('Oh no! Something when wrong. Please try again.')
+  })
+}
