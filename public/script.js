b543f2222cfbc1c6612d2d5b27e3dea08c66063e
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-08-03 14:54:52 -0700

    Remove edge case

--- public/script.js
+++ public/script.js
@@ -1,113 +1,8 @@
 const baseURL = 'http://mutably.herokuapp.com'
 
-$(document).ready(function(){
+// Functions for updating the view
 
-  getAllBooks(baseURL)
-
-  $(document).on('click', '.view-btn', function() {
-    const bookId = $(this).closest('li').attr('id')
-    if ($('.show-card').length) {
-      $('.show-card').remove()
-    } else {
-      getOneBook('view', bookId)
-    }
-  })
-
-  $(document).on('click', '.close-btn', function() {
-    $(this).closest('div.show-card').remove()
-  })
-
-  $(document).on('click', '.delete-btn', function() {
-    const bookId = $(this).closest('li').attr('id')
-    deleteOneBook(bookId)
-  })
-
-  $(document).on('click', '.edit-btn', function() {
-    const bookId = $(this).closest('li').attr('id')
-    if ($('.edit-card').length) {
-      $('.edit-card').remove()
-    } else {
-      getOneBook('edit', bookId)
-    }
-  })
-
-  $(document).on('click', '.submit-edit-btn', function(event) {
-    event.preventDefault()
-    const bookId = $(this).closest('div.edit-card').prev().attr('id')
-    const image = $(this).closest('div.edit-card').children().children('.image-holder').children().attr('src')
-    const title = $(this).parent().parent().find('input#title').val()
-    const author = $(this).parent().parent().find('input#author').val()
-    const releaseDate = $(this).parent().parent().find('input#date').val()
-    const details = {
-      title,
-      author,
-      releaseDate,
-      image
-    }
-    editBookDetails(bookId, details)
-    $(document).find('div.edit-card').remove()
-  })
-
-  $(document).on('click', '.add-btn', function() {
-    $('#myModal').modal('show');
-  })
-
-  $(document).on('click', '.add-new-submit', function(event) {
-    event.preventDefault()
-    const image = $(this).parent().prev().find('input#image').val()
-    const title = $(this).parent().prev().find('input#title').val()
-    const author = $(this).parent().prev().find('input#author').val()
-    const releaseDate = $(this).parent().prev().find('input#date').val()
-    const details = {
-      title,
-      author,
-      releaseDate,
-      image
-    }
-
-    addNewBook(details)
-  })
-})
-
-// Grab all books from api
-const getAllBooks = () => {
-  $.ajax({
-    url: `${baseURL}/books`,
-    contentType: 'application/json',
-    method: 'get'
-  })
-  .done(function(response) {
-    populateList(response.books)
-  })
-  .catch(error => {
-    alert('Oh no! Something went wrong. Please try again.')
-  })
-}
-
-// Grab one book from api
-const getOneBook = (btn, bookId) => {
-  $.ajax({
-    url: `${baseURL}/books/${bookId}`,
-    contentType: 'application/json',
-    method: 'get'
-  })
-  .done(function(book) {
-    if (btn === 'view') {
-      createViewBookCard(book)
-    } else {
-      createEditBookCard(book)
-    }
-  })
-}
-
-// Create a new list element for each book
-const populateList = (books) => {
-  books.forEach(function(book) {
-    createListItem(book)
-  })
-}
-
-const createListItem = (book) => {
+const addListItem = (book) => {
   $('.list-group').append(`
     <li class='list-group-item' id=${book._id}>
       <p class='list-title'>${book.title}</p>
@@ -120,8 +15,15 @@ const createListItem = (book) => {
   `)
 }
 
+// Create a new list element for each book
+const populateList = (books) => {
+  books.forEach((book) => {
+    addListItem(book)
+  })
+}
+
 // Dynamically add a view card for the fetched book
-const createViewBookCard = (book) => {
+const addViewBookCard = (book) => {
   $(document).find(`#${book._id}`).after(`
     <div class='show-card'>
       <div class='row'>
@@ -142,11 +44,11 @@ const createViewBookCard = (book) => {
 }
 
 // Dynamically add an edit card for a fetched book
-const createEditBookCard = (book) => {
+const addEditBookCard = (book) => {
   $(document).find(`#${book._id}`).after(`
     <div class='edit-card'>
       <div class='row'>
-        <div class='col-sm-3 image-holder'>
+        <div class='col-sm-3 image-box'>
           <img class='thumbnail' src=${book.image} alt='Card image cap' height='230px' width='160px'>
         </div>
         <div class='col-sm-9'>
@@ -171,6 +73,9 @@ const createEditBookCard = (book) => {
               </div>
             </div>
             <div class='edit-btns'>
+              <div class='input-section'>
+                <input type='hidden' name='book-image' role='uploadcare-uploader' id='image-'></input>
+              </div>
               <button type='submit' class='btn btn-primary form-btn upload-btn'>Upload Image</button>
               <button type='submit' class='btn btn-primary form-btn submit-edit-btn'>Submit</button>
             </div>
@@ -192,45 +97,77 @@ const updateSingleListElement = (bookId, updatedBook) => {
   `).attr(updatedBook._id)
 }
 
+// TODO edge cases
+const removeBookElement = (bookId) => {
+  $(`#${bookId}`).hide('slow', function () {
+    $(this).remove()
+  })
+}
+
+// Functions for interacting with the API
+// Grab all books from api
+const getAllBooks = () => {
+  $.ajax({
+    url: `${baseURL}/books`,
+    contentType: 'application/json',
+    method: 'get',
+  })
+    .done((response) => {
+      populateList(response.books)
+    })
+    .catch((error) => {
+      alert('Oh no! Something went wrong. Please try again.')
+    })
+}
+
+// Grab one book from api
+const getOneBook = (btn, bookId) => {
+  $.ajax({
+    url: `${baseURL}/books/${bookId}`,
+    contentType: 'application/json',
+    method: 'get',
+  })
+    .done((book) => {
+      if (btn === 'view') {
+        addViewBookCard(book)
+      } else {
+        addEditBookCard(book)
+      }
+    })
+}
+
 // Delete book entry from the page
 const deleteOneBook = (bookId) => {
   $.ajax({
     url: `${baseURL}/books/${bookId}`,
     contentType: 'application/json',
-    method: 'delete'
+    method: 'delete',
   })
-  .done(function(response) {
-    removeBookElement(bookId)
-  })
-  .catch(error => {
-    console.error('Error: ', error)
-    alert('Oh no! Something when wrong. Please try again.')
-  })
-}
-
-// TODO edge cases
-const removeBookElement = (bookId) => {
-  $(`#${bookId}`).hide('slow', function() {
-    $(this).remove();
-  })
+    .done(() => {
+      removeBookElement(bookId)
+    })
+    .catch((error) => {
+      console.error('Error: ', error)
+      alert('Oh no! Something when wrong. Please try again.')
+    })
 }
 
 // Edit book details
-const editBookDetails = (bookId, updatedDetails) => {
+const updateBookDetails = (bookId, updatedDetails) => {
   $.ajax({
     url: `${baseURL}/books/${bookId}`,
     contentType: 'application/json',
     method: 'put',
-    data: JSON.stringify(updatedDetails)
+    data: JSON.stringify(updatedDetails),
   })
-  .done(function(updatedBook) {
-    alert('Book updated successfully!')
-    updateSingleListElement(bookId, updatedBook)
-  })
-  .catch(error => {
-    console.error('Error: ', error)
-    alert('Oh no! Something when wrong. Please try again.')
-  })
+    .done((updatedBook) => {
+      alert('Book updated successfully!')
+      updateSingleListElement(bookId, updatedBook)
+    })
+    .catch((error) => {
+      console.error('Error: ', error)
+      alert('Oh no! Something when wrong. Please try again.')
+    })
 }
 
 // Add a new book
@@ -239,14 +176,85 @@ const addNewBook = (bookDetails) => {
     url: `${baseURL}/books`,
     contentType: 'application/json',
     method: 'post',
-    data: JSON.stringify(bookDetails)
+    data: JSON.stringify(bookDetails),
   })
-  .done(function(savedBook) {
-    createListItem(savedBook)
-    $('#myModal').modal('hide')
-  })
-  .catch(error => {
-    console.error('Error: ', error)
-    alert('Oh no! Something when wrong. Please try again.')
-  })
+    .done((savedBook) => {
+      addListItem(savedBook)
+      $('#myModal').modal('hide')
+    })
+    .catch((error) => {
+      console.error('Error: ', error)
+      alert('Oh no! Something when wrong. Please try again.')
+    })
 }
+
+// Controllers
+
+$(document).ready(() => {
+  getAllBooks(baseURL)
+
+  $(document).on('click', '.view-btn', function () {
+    const bookId = $(this).closest('li').attr('id')
+    if ($('.show-card').length) {
+      $('.show-card').remove()
+    } else {
+      getOneBook('view', bookId)
+    }
+  })
+
+  $(document).on('click', '.close-btn', function () {
+    $(this).closest('div.show-card').remove()
+  })
+
+  $(document).on('click', '.delete-btn', function () {
+    const bookId = $(this).closest('li').attr('id')
+    $(document).find('.edit-card').remove()
+    $(document).find('.show-card').remove()
+    deleteOneBook(bookId)
+  })
+
+  $(document).on('click', '.edit-btn', function () {
+    const bookId = $(this).closest('li').attr('id')
+    if ($('.edit-card').length) {
+      $('.edit-card').remove()
+    } else {
+      getOneBook('edit', bookId)
+    }
+  })
+
+  $(document).on('click', '.submit-edit-btn', function (event) {
+    event.preventDefault()
+    const bookId = $(this).closest('div.edit-card').prev().attr('id')
+    const image = $(this).closest('div.edit-card').children().children('.image-holder').children().attr('src')
+    const title = $(this).parent().parent().find('input#title').val()
+    const author = $(this).parent().parent().find('input#author').val()
+    const releaseDate = $(this).parent().parent().find('input#date').val()
+    const details = {
+      title,
+      author,
+      releaseDate,
+      image,
+    }
+    updateBookDetails(bookId, details)
+    $(document).find('div.edit-card').remove()
+  })
+
+  $(document).on('click', '.add-btn', () => {
+    $('#myModal').modal('show')
+  })
+
+  $(document).on('click', '.add-new-submit', function (event) {
+    event.preventDefault()
+    const image = $(this).parent().prev().find('input#image').val()
+    const title = $(this).parent().prev().find('input#title').val()
+    const author = $(this).parent().prev().find('input#author').val()
+    const releaseDate = $(this).parent().prev().find('input#date').val()
+    const details = {
+      title,
+      author,
+      releaseDate,
+      image,
+    }
+    addNewBook(details)
+  })
+})
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express')
+
 const app = express()
-
 app.use(express.static('public'))
+const ejs = require('ejs')
 
-// set 'html' as the engine, using ejs's renderFile function
-var ejs = require('ejs');
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'html');
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'html')
 
 app.get('/', (request, response) => {
-  response.render('index')
+    response.render('index')
 })
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
+    console.log(`http://localhost:${port}`)
 })
