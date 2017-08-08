467f7406789d1af02d9ed23c33188ed092925939
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-08 14:54:11 -0700

    AJAX retrieves book list (and console.logs it)

--- public/script.js
+++ public/script.js
@@ -5,10 +5,30 @@ $('button.editButton').on('click', () => {
   $('button.saveButton').removeClass('invisible')
 })
 
-$('button.saveButton').on('click', () => {
-  // $('button.saveButton').toggle()
-  $('button.saveButton').addClass('invisible')
-  $('button.editButton').removeClass('invisible')
+// for(i=0; i<$(button.saveButton).length i++){
+  $('button.saveButton').on('click', () => {
+    // $('button.saveButton').toggle()
+    $('button.saveButton').addClass('invisible')
+    $('button.editButton').removeClass('invisible')
+    // console.log($('button.editButton')[0])
+  })
+// }
+
+$()
+
+const testRun = $.getJSON('https://mutably.herokuapp.com/books')
+console.log('keys:', Object.keys(testRun))
+console.log('testRun:', testRun)
+
+const getStuff = $.ajax({
+  type: 'GET',
+  url: 'https://mutably.herokuapp.com/books',
+  success: (data) => {
+    console.log('success:', data.books[0].title)
+  }
 })
 
+// createBookRow = () => {
+
+}
 });
