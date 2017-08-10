b25cbe6dfee61ade348cc5fc1de220d9fb897a8f
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-10 15:30:24 -0700

    Adds ability to edit book titles and authors

--- public/script.js
+++ public/script.js
@@ -20,18 +20,14 @@ displayAllBooks = (mutablyResponse) => {
   }
 }
 
-const showAllBooks = () => {
-  $.ajax({
-    type: 'GET',
-    url: 'https://mutably.herokuapp.com/books',
-    success: (mutablyResponse) => {
-      displayAllBooks(mutablyResponse)
-      eventListeners()
-    }
-  })
-}
-
-showAllBooks()
+$.ajax({
+  type: 'GET',
+  url: 'https://mutably.herokuapp.com/books',
+  success: (mutablyResponse) => {
+    displayAllBooks(mutablyResponse)
+    eventListeners()
+  }
+})
 
 const addBook = (title, author, bookId) => {
   event.preventDefault()
@@ -51,25 +47,53 @@ const deleteBook = (bookId) => {
   })
 }
 
-$('button.addBook').on('click', () => {
-  const newBookForm = $('.newBook').serializeArray()
+const updateBook = (title, author, bookId) => {
+  $.ajax({
+    type: 'PUT',
+    url: 'https://mutably.herokuapp.com/books/' + bookId,
+    data: {title, author},
+    success: () => {
+      console.log('success')
+      // $('li[data-book-id="'+bookId+'"]').remove()
+      // createBookRow(title, author, bookId)
+    }
+  })
+}
 
-  const newBookTitle = newBookForm[0].value
-  const newBookAuthor = newBookForm[1].value
 
-  addBook(newBookTitle, newBookAuthor)
-})
 
 const eventListeners = () => {
+  $('button.addBook').on('click', () => {
+    const newBookForm = $('.newBook').serializeArray()
+
+    const newBookTitle = newBookForm[0].value
+    const newBookAuthor = newBookForm[1].value
+
+    addBook(newBookTitle, newBookAuthor)
+  })
+
   $('.deleteButton').on('click', function () {
     const bookId = $(this).parent().attr('data-book-id')
     deleteBook(bookId)
+  })
+
+
+  $('button.saveButton').on('click', function () {
+    // $(this).toggleClass('invisible')
+    const bookId = $(this).parent().attr('data-book-id')
+    const bookTitle = $(this).siblings('input')[0].value
+    const bookAuthor = $(this).siblings('input')[1].value
+    updateBook(bookTitle, bookAuthor, bookId)
+
+    $(this).siblings('p')[0].innerText = bookTitle
+    $(this).siblings('p')[1].innerText = bookAuthor
   })
 
   $('button.editButton, button.saveButton').on('click', () => {
     $('button.editButton, button.saveButton').toggleClass('invisible')
     $('p, input').toggleClass('invisible')
   })
+
 
 }
 
