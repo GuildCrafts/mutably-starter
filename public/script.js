46f896ed883c06fe6cb684ed5ad5ef918b7d011a
eobaah <eobaah@gmail.com>
2017-09-05 14:31:04 -0700

    test fetch for albums

--- public/script.js
+++ public/script.js
@@ -1,7 +1,31 @@
 console.log("Sanity Check: JS is working!");
-
-$(document).ready(function(){
-
-  // code in here
-
+const getAlbums = document.querySelector('.get-albums')
+const listGroup = document.querySelector('.list-group')
+getAlbums.addEventListener("click", function(event){
+    // event.preventDefault()
+    getAllAlbums
 });
+
+function getAllAlbums() {
+  // prevent.default()
+  alert("it works!")
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+
+  fetch('http://mutably.herokuapp.com/albums', {
+    method: 'GET',
+    mode: 'cors',
+    headers: headers
+  }).then( albums => {
+    albums = albums.json()
+    return albums
+  }).then(albums => {
+  console.log(albums[0])
+  });
+}
+
+// $(document).ready(function(){
+//
+//
+//
+// });
