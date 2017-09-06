d20478b1860c913c1135ca44cb75b2ac8b5f337f
Jacklyne Bouier <jacky_bouier@yahoo.com>
2017-09-06 15:47:50 -0700

    added a input button

--- public/script.js
+++ public/script.js
@@ -8,21 +8,22 @@ $(document).ready(function(){
   fetch("http://mutably.herokuapp.com/books").then(function(response){
     return response.json()
   }).then(function(bookData){
-    for (var i in bookData) {
-    for (var j in bookData[i]){
+     for (var i in bookData) {
+     for (var j in bookData[i]){
         // console.log(bookData[i][j]);
         listOfBooks.push(bookData[i][j].title + " " + "Author " + bookData[i][j].author)
     }
+
     for (var i in listOfBooks) {
-    $('.list-group').append( '<li>' + listOfBooks[i] + '</li>' );
-}
+     $('.list-group').append( '<li>' + listOfBooks[i] + '</li>' );
+  }
+ }
+})
 
-
-}
-  console.log(listOfBooks)
-
-
-  })
+$('.panel-heading').empty()
+$('.panel-heading').append('<label for="fname">Enter Book</label> <input type="text" name="something" id="something" placeholder="Enter Book" />');
+$('.panel-heading').append('<label for="fname">Enter Author</label> <input type="text" name="something" id="something" placeholder="Enter Author" />');
+$('.panel-heading').append('<input type="button" value="Add Book">');
 
 
 
