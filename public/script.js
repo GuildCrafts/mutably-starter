20f153187fb991d63956fc9f7752a42c94955395
Jacklyne Bouier <jacky_bouier@yahoo.com>
2017-09-06 13:13:01 -0700

    pulling data from books api and displaying on page

--- public/script.js
+++ public/script.js
@@ -3,5 +3,14 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   // code in here
+  fetch("http://mutably.herokuapp.com/books").then(function(response){
+    return response.json()
+  }).then(function(j){
+    console.log(j)
+
+    document.querySelector(".list-group").innerText = JSON.stringify(j);
+  })
+
+
 
 });
