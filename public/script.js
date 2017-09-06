44fb57f6e33b91fd1d10710ba2473b11eae9088e
diop <fodesdiop@gmail.com>
2017-09-06 00:45:43 -0700

    scaffolding for JQuery/Ajax structure

--- public/script.js
+++ public/script.js
@@ -4,19 +4,33 @@ const listGroup = document.querySelector('.list-group')
 
 $(document).ready(function(){
   getAllAlbums()
+
+  $(document).on('click', '.save-btn', function() {
+
+    })
+
+  $(document).on('click', '.edit-btn', function() {
+
+    })
+
+  $(document).on('click', '.delete-btn', function() {
+      $.ajax({
+        method: 'DELETE',
+        url: url + id,
+        success: deleteAlbum
+      })
+    })
 });
 
 const checkStatus = response =>  {
   if (response.status === 200) {
     return Promise.resolve(response)
   } else {
-    return Promise.reject(
-      new Error(response.statusText)
-    )
+    return Promise.reject(new Error(response.statusText))
   }
 }
 
-const getJSON =  response => {
+const getJSON = response => {
   return response.json()
 }
 
