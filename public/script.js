b1043e5972e6b1312e4d95368eff865d0279c922
Jacklyne Bouier <jacky_bouier@yahoo.com>
2017-09-06 14:30:58 -0700

    retrieving the book title and the author names

--- public/script.js
+++ public/script.js
@@ -3,12 +3,21 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   // code in here
+  var listOfBooks = [];
+
   fetch("http://mutably.herokuapp.com/books").then(function(response){
     return response.json()
-  }).then(function(j){
-    console.log(j)
+  }).then(function(bookData){
+    for (var i in bookData) {
+    for (var j in bookData[i]){
+        console.log(bookData[i][j]);
+        listOfBooks.push(bookData[i][j].title + " " + "Author " + bookData[i][j].author)
+    }
+    document.querySelector(".list-group").innerText = JSON.stringify(listOfBooks)
+}
+  console.log(listOfBooks)
 
-    document.querySelector(".list-group").innerText = JSON.stringify(j);
+    
   })
 
 
