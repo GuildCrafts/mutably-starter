3eb619421131553e55084b7b8ec247b2154523dd
Jacklyne Bouier <jacky_bouier@yahoo.com>
2017-09-06 18:20:33 -0700

    post book data to database

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-console.log("Sanity Check: JS is working!");
+console.log('Sanity Check: JS is working!');
 
 $(document).ready(function(){
 
@@ -21,10 +21,30 @@ $(document).ready(function(){
 })
 
 $('.panel-heading').empty()
-$('.panel-heading').append('<label for="fname">Enter Book</label> <input type="text" name="something" id="something" placeholder="Enter Book" />');
-$('.panel-heading').append('<label for="fname">Enter Author</label> <input type="text" name="something" id="something" placeholder="Enter Author" />');
-$('.panel-heading').append('<input type="button" value="Add Book">');
+$('.panel-heading').append('<label for="fname">Enter Book</label> <input id="book" type="text" name="something" id="something" placeholder="Enter Book" />');
+$('.panel-heading').append('<label for="fname">Enter Author</label> <input id="author" type="text" name="something" id="something" placeholder="Enter Author" />');
+$('.panel-heading').append('<input class="addBooks" id="main" type="button" value="Add Book">');
 
 
+
+
+
+$('#main').click(function() {
+fetch('http://mutably.herokuapp.com/books', {
+	method: 'post',
+  headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json'
+  },
+	body: JSON.stringify({
+		title: document.getElementById('book').value,
+		author: document.getElementById('author').value
+	})
+}).then(response => response.json())
+  .then(response => console.log(response));
+
+
+
+});
 
 });
