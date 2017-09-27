8b8f1ef4b84a7662f424d43798c3a961cf880113
alexandrawaite <alexandrawaite@gmail.com>
2017-09-27 11:36:14 -0700

    create initial get request for /books that reads all books

--- public/script.js
+++ public/script.js
@@ -1,9 +1,25 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  const createNode = element => document.createElement(element);
+
+  const append = (parent, element) => parent.appendChild(element);
+
+  const ul = document.querySelector('.list-group');
+
   fetch('https://mutably.herokuapp.com/books')
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      let books = data.books;
+      return books.map(book => {
+        let li = createNode('li'),
+            img = createNode('img'),
+            span = createNode('span');
+        img.src = book.image;
+        span.innerHTML = `${book.author} ${book.title} ${book.releaseDate}`
+        append(li, img);
+        append(li, span);
+        append(ul, li);
+      })
     })
 });
